f4daaef16c37b75983160b1b85992e015e8b4f51
ecabigting <ericcabigting@outlook.com>
2024-11-14 18:20:55 +0400

    update

--- vmstart.sh
+++ vmstart.sh
@@ -3,4 +3,4 @@ echo " --Starting virtual machine --"
 virsh --connect qemu:///system start win11-base
 sleep 50
 echo "-- Sleeping for 50 seconds before connecting to device --"
-xfreerdp3 -grab-keyboard /v:192.168.122.225 /size:100% /u: /p: /f /cert:ignore /d: /dynamic-resolution /gfx:avc444,progressive:on &
+xfreerdp3 -grab-keyboard /v:192.168.122.225 /size:100% /u: /p: /cert:ignore /d: /dynamic-resolution /gfx:avc444,progressive:on &
