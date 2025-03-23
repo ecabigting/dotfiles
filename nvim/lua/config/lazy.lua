dffd07fa027aa3b08687d29549e91fc14db96be7
ecabigting <ericcabigting@outlook.com>
2025-03-23 20:38:39 +0400

    update nvim config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "plugins" },
   },
   defaults = {
