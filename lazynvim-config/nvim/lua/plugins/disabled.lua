efa16dd0c6e0072e363fd9ff8c1a2360c79a552a
ecabigting <ericcabigting@outlook.com>
2025-04-10 22:22:53 +0400

    update

--- lazynvim-config/nvim/lua/plugins/disabled.lua
+++ lazynvim-config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "folke/tokyonight.nvim", lazy = true, enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", lazy = true, enabled = false },
+  { "folder/flash.nvim", lazy = true, enabled = false },
 }
