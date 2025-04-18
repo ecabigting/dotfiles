625558a52d286c514e8cff1e8de8fa8598c1076e
ecabigting <ericcabigting@outlook.com>
2025-04-18 16:03:37 +0400

    clear out nvim configs

--- lazynvim-config/nvim/lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "folke/tokyonight.nvim", lazy = true, enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", lazy = true, enabled = false },
-  { "folder/flash.nvim", lazy = true, enabled = false },
-}
