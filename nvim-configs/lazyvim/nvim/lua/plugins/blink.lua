f366cfa0092ac06129ed79b68f938c3887eefb5b
ecabigting <ericcabigting@outlook.com>
2025-04-18 16:10:14 +0400

    new nvim config

---
+++ nvim-configs/lazyvim/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    sources = {
+      -- adding any nvim-cmp sources here will enable them
+      -- with blink.compat
+      -- compat = { name = "buffer", keyword_length = 9999 },
+      default = function()
+        return { "snippets", "lsp", "path" }
+      end,
+    },
+  },
+}
