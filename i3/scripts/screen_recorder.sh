9c5b77b5f273697a648d621a7a08274a7eeb62a4
ecabigting <ericcabigting@outlook.com>
2025-04-27 01:40:40 +0400

    screen_recorder
    debugging:
    - for audio check with `aplay -l` and `pactl info` to get details audio
    setup

---
+++ i3/scripts/screen_recorder.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PID_FILE="/tmp/screen_recorder.pid"
+
+# Function to start recording
+start_recording() {
+  slop=$(slop -f "%x %y %w %h %g %i") || exit 1
+  read -r X Y W H G ID <<<"$slop"
+  # Ensure width and height are even numbers
+  W=$((W + W % 2)) # Round up to the nearest even number
+  H=$((H + H % 2)) # Round up to the nearest even number
+
+  # Get the default sink name
+  DEFAULT_SINK=$(pactl info | grep 'Default Sink' | cut -d ' ' -f3)
+  MONITOR_SOURCE="${DEFAULT_SINK}.monitor"
+
+  # Start ffmpeg in the background and save its PID
+  ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f pulse -i "$MONITOR_SOURCE" -c:v libx264 -preset ultrafast -pix_fmt yuv420p -c:a aac -b:a 192k -async 1 ~/Videos/$(date +%Y-%m-%d-%T)-screen-recording.mp4 &
+  echo $! >"$PID_FILE"
+  notify-send "Screen Recording" "Recording started."
+}
+
+# Function to stop recording
+stop_recording() {
+  if [ -f "$PID_FILE" ]; then
+    kill "$(cat "$PID_FILE")"
+    rm "$PID_FILE"
+    notify-send "Screen Recording" "Recording stopped and saved."
+  else
+    echo "No screen recording is currently running."
+  fi
+}
+
+# Check if the recording is already in progress
+if [ -f "$PID_FILE" ]; then
+  stop_recording
+else
+  start_recording
+fi
