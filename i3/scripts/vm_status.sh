27bbdb95497ae721e19562fca99a6311dcc7bc6e
ecabigting <thomas@ericcabigting.dev>
2025-05-18 21:50:35 +0400

    update config
    zen main browser

--- i3/scripts/vm_status.sh
+++ i3/scripts/vm_status.sh
@@ -6,6 +6,10 @@ VM_IP="192.168.1.24" # Replace this with the actual IP address of your VM
 # Set the name of your VM as it appears in Virt-Manager
 VM_NAME="win11-base" # Replace this with the actual name of your VM
 
+# colors
+color="#cad3f5"
+bgcolor="#24273a"
+vmStatus="<span>󰍹 </span>"
 # Detect click events
 case $BLOCK_BUTTON in
 1) # Left click
@@ -29,8 +33,15 @@ esac
 # Ping the VM to check if it's online (with a timeout)
 if ping -c 1 -W 1 "$VM_IP" &>/dev/null; then
   # If online, display a green computer icon
-  echo "<span color='green'>󰍹</span>" # Nerd Font icons for computer and checkmark
+  # echo "<span color='green'>󰍹</span>" # Nerd Font icons for computer and checkmark
+  bgcolor="#b7bdf8"
+
 else
   # If offline, display a red computer icon
-  echo "<span color='red'>󰶐</span>" # Nerd Font icons for computer and cross
+  # echo "<span color='red'>󰶐</span>" # Nerd Font icons for computer and cross
+  bgcolor="#24273a"
 fi
+
+echo ${vmStatus}
+echo ${color}
+echo ${bgcolor}
