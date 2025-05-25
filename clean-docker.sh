24ae25a06dd37368e4de142816c918704eceee13
ecabigting <thomas@ericcabigting.dev>
2025-05-25 14:16:24 +0400

    update cleaning scripts

--- clean-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#docker system prune -af --volumes &
-#docker volume rm $(docker volume ls -q)
-
-docker system prune -af &&
-  docker image prune -af &&
-  docker system prune -af --volumes &&
-  docker system df
