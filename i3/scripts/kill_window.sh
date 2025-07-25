4817183f3d04f3fc26fef120147d19f9c8161f3a
ecabigting <thomas@ericcabigting.dev>
2025-07-25 20:46:01 +0400

    updated i3 config

--- i3/scripts/kill_window.sh
+++ i3/scripts/kill_window.sh
@@ -3,7 +3,7 @@ active_window_class=$(xdotool getactivewindow getwindowclassname)
 
 # Specify the application you want to protect
 protected_app1="xfreerdp"
-protected_app2="aocli.exe"
+protected_app2="steam_proton"
 
 # Check if the active window is not one of the protected applications
 if [[ "$active_window_class" != *"$protected_app1"* && "$active_window_class" != *"$protected_app2"* ]]; then
