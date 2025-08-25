371cae84e622debd736c75119c039c8278eb5e79
ecabigting <thomas@ericcabigting.dev>
2025-08-25 21:54:11 +0400

    update

--- i3/scripts/vm_status.sh
+++ i3/scripts/vm_status.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Set the IP address of your Windows VM
-VM_IP="192.168.1.24" # Replace this with the actual IP address of your VM
+VM_IP="xxx.xxx.xx.xxx" # Replace this with the actual IP address of your VM
 
 color="#cad3f5"
 bgcolor="#1e203080"
-vmStatus="<span>󰍹 </span>"
+vmStatus="<span>󰶐</span>"
 
 # Ping the VM to check if it's online (with a timeout)
 if ping -c 1 -W 1 "$VM_IP" &>/dev/null; then
   # If online, display a green computer icon
   # echo "<span color='green'>󰍹</span>" # Nerd Font icons for computer and checkmark
   color="#a6da95"
-  vmStatus="<span>󱄄 </span>"
+  vmStatus="<span>󰨇</span>"
 fi
 
 echo ${vmStatus}
