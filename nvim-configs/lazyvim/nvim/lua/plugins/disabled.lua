529aed2f3572aef60ab1b272b30574197a9e4bf7
ecabigting <thomas@ericcabigting.dev>
2025-10-14 09:47:33 +0400

    update

--- nvim-configs/lazyvim/nvim/lua/plugins/disabled.lua
+++ nvim-configs/lazyvim/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", lazy = true, enabled = false },
   -- { "folke/flash.nvim", lazy = true, enabled = false },
-  { "echasnovski/mini.nvim", enabled = false },
-  { "echasnovski/mini.ai", enabled = false },
+  { "nvim-mini/mini.nvim", enabled = false },
+  { "nvim-mini/mini.ai", enabled = false },
 }
